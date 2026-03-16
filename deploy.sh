4a169416f8fb5b38d4142a0436a9e84190047e69
Jake <jake@jakefixestech.com>
2026-03-15 22:21:14 -0400

    update

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,3 @@
 git add .
 git commit -m "update"
 git push
-```
-
-Then in your terminal run this once to make it executable:
-```
-chmod +x deploy.sh
-```
-
-After that, whenever you want to deploy just run:
-```
-./deploy.sh
